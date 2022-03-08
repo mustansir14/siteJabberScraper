a60207b20833a98b7ebe23786fb4526244008ed5
systemaworking <69110904+systemaworking@users.noreply.github.com>
2022-03-08 13:28:58 +0200

    test commit

--- schema.sql
+++ schema.sql
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS `company` (
   PRIMARY KEY (`company_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
+
 CREATE TABLE IF NOT EXISTS `review` (
   `review_id` int NOT NULL AUTO_INCREMENT,
   `company_id` varchar(255) NOT NULL,
